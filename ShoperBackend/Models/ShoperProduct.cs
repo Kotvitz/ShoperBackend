8684d14a77ffe2bb36aea995c672cc03c7b6f341
Michał Kotfica <kotvitz17@gmail.com>
2025-02-17 21:45:43 +0100

    fix: Add missing JsonPropertyName attribute in ShoperProduct

--- ShoperBackend/Models/ShoperProduct.cs
+++ ShoperBackend/Models/ShoperProduct.cs
@@ -1,8 +1,13 @@
-﻿namespace ShoperBackend.Models
+﻿using System.Text.Json.Serialization;
+
+namespace ShoperBackend.Models
 {
     public class ShoperProduct
     {
+        [JsonPropertyName("product_id")]
         public required string ProductId { get; set; }
+
+        [JsonPropertyName("translations")]
         public required ShoperTranslations Translations { get; set; }
     }
 }
