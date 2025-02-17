bdc38740f75678a027ffc92c150570f489a0ef78
Michał Kotfica <kotvitz17@gmail.com>
2025-02-17 22:24:15 +0100

    feat: Add ProductsController with search endpoint

---
+++ ShoperBackend/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ShoperBackend.Services;
+
+namespace ShoperBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController(IProductService productService) : ControllerBase
+    {
+        private readonly IProductService _productService = productService;
+
+        [HttpGet("search")]
+        public async Task<IActionResult> Search([FromQuery] string query)
+        {
+            if (string.IsNullOrWhiteSpace(query) || query.Length < 3)
+            {
+                return BadRequest("Query must be at least 3 characters long.");
+            }
+
+            var products = await _productService.SearchProductsAsync(query);
+            return Ok(products);
+        }
+    }
+}
